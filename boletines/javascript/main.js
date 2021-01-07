e8a519dfe7df262bc0890b80006e7813456a83bf
Fernando-Mendozamm <fernando_mendoza52@hotmail.com>
2021-01-07 16:18:16 -0600

    version 2

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -3,15 +3,15 @@ links.forEach(function(link){
     //Agregar un evento click a cada uno de ellos
      link.addEventListener("click",function(ev){
         ev.preventDefault();
-        let content = document.querySelector('.content');
+        let links = document.querySelector('.close');
 
         //Quitarle las clases de animacion que ya tiene
-        content.classList.remove("fadeInDown");
-        content.classList.remove("animated");
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
 
         //Agregar clases para animar su saludo fadeOutUp
-        content.classList.add("fadeOutUp");
-        content.classList.add("animated");
+        content.classList.add("animate__fadeInDownfadeOutUp");
+        content.classList.add("animate__animated");
 
      return false;
     });
